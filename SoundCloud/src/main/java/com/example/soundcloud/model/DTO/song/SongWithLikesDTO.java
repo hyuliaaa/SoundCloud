566fd1a936e7eca98efff151af752f6cef67232d
hyuliaaa <hulia_99@abv.bg>
2022-02-20 17:35:20 +0200

    Modified like and unlike of a song

---
+++ SoundCloud/src/main/java/com/example/soundcloud/model/DTO/song/SongWithLikesDTO.java
@@ -0,0 +1,18 @@
+package com.example.soundcloud.model.DTO.song;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class SongWithLikesDTO {
+
+    private long id;
+    private String title;
+    private LocalDateTime uploadedAt;
+    private int views;
+    private String songUrl;
+    private String coverPhotoUrl;
+    private boolean isPublic;
+    private int numberOflikes;
+}
