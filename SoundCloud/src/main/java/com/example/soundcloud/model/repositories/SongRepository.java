a306ecbe3394aa13c7d2579abfbae916e54ecd6c
hyuliaaa <hulia_99@abv.bg>
2022-02-20 13:37:14 +0200

    Added uploading a picture to a song

--- SoundCloud/src/main/java/com/example/soundcloud/model/repositories/SongRepository.java
+++ SoundCloud/src/main/java/com/example/soundcloud/model/repositories/SongRepository.java
@@ -3,8 +3,10 @@ package com.example.soundcloud.model.repositories;
 import com.example.soundcloud.model.entities.Song;
 import com.example.soundcloud.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
