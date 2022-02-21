9a5195df92f123dfb873ef25dbb8fcd553f3865d
hyuliaaa <hulia_99@abv.bg>
2022-02-21 14:50:59 +0200

    Added create playlist

--- SoundCloud/src/main/java/com/example/soundcloud/controller/PlaylistController.java
+++ SoundCloud/src/main/java/com/example/soundcloud/controller/PlaylistController.java
@@ -1,13 +1,33 @@
 package com.example.soundcloud.controller;
 
+import com.example.soundcloud.model.DTO.playlist.PlaylistCreateRequestDTO;
+import com.example.soundcloud.model.DTO.playlist.PlaylistResponseDTO;
+import com.example.soundcloud.model.DTO.song.SongUploadRequestDTO;
+import com.example.soundcloud.model.DTO.song.SongWithoutUserDTO;
 import com.example.soundcloud.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import static com.example.soundcloud.controller.UserController.USER_ID;
 
 @RestController
 public class PlaylistController {
 
     @Autowired
     private PlaylistService playlistService;
+
+    @PostMapping("/create_playlist")
+    ResponseEntity<PlaylistResponseDTO> createPlaylist(@Valid @RequestBody PlaylistCreateRequestDTO createPlaylistDTO, HttpSession session){
+        long id = (long) session.getAttribute(USER_ID);
+        return new ResponseEntity<>(playlistService.createPlaylist(id, createPlaylistDTO), HttpStatus.CREATED);
+    }
+
 }
 
--- SoundCloud/src/main/java/com/example/soundcloud/service/PlaylistService.java
+++ SoundCloud/src/main/java/com/example/soundcloud/service/PlaylistService.java
@@ -1,9 +1,19 @@
 package com.example.soundcloud.service;
 
+import com.example.soundcloud.model.DTO.playlist.PlaylistCreateRequestDTO;
+import com.example.soundcloud.model.DTO.playlist.PlaylistResponseDTO;
+import com.example.soundcloud.model.entities.Playlist;
+import com.example.soundcloud.model.entities.User;
 import com.example.soundcloud.model.repositories.PlaylistRepository;
+import com.example.soundcloud.model.repositories.UserRepository;
+import com.example.soundcloud.util.Utils;
 import lombok.Data;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
 
 @Service
 @Data
@@ -11,4 +21,26 @@ public class PlaylistService {
 
     @Autowired
     private PlaylistRepository playlistRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private Utils utils;
+
+    public PlaylistResponseDTO createPlaylist(long id, @Valid PlaylistCreateRequestDTO createPlaylistDTO) {
+        PlaylistResponseDTO dto = new PlaylistResponseDTO();
+        Playlist playlist = new Playlist();
+        modelMapper.map(createPlaylistDTO,playlist);
+        playlist.setCreatedAt(LocalDateTime.now());
+        playlist.setLastModified(LocalDateTime.now());
+        playlist.setOwner(utils.getUserById(id));
+        playlistRepository.save(playlist);
+        modelMapper.map(playlist,dto);
+        dto.setOwnerId(id);
+        return dto;
+    }
 }
